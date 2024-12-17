1285effdad6789800f96bcef5938209f4d881b9e
Yansong Zhao <bill.zhaoyansong@gmail.com>
2024-12-17 15:30:40 +0800

    styling labels

--- src/app/papers/page.tsx
+++ src/app/papers/page.tsx
@@ -12,6 +12,8 @@ export default async function Papers() {
 
     const paperJsonList = []
 
+    const labelSet = new Set<string>()
+
     for (let paper of papers) {
         const paperPath = path.join(process.cwd(), 'src', 'app', 'papers', 'paper-list', paper); // Specify your directory  
 
@@ -19,6 +21,12 @@ export default async function Papers() {
         const data = JSON.parse(jsonData);
 
         paperJsonList.push(data)
+
+        if (Object.hasOwn(data, 'labels')) {
+            data['labels'].forEach((label: string) => {
+                labelSet.add(label);
+            });
+        }
     }
 
     // console.log(paperJsonList)
@@ -26,18 +34,41 @@ export default async function Papers() {
 
 
 
-    return <div className="flex flex-row">
-        <div className="flex-1 h-screen overflow-auto flex flex-col">
-            <div className="bg-yellow-100 px-9 py-6 flex-initial">
-                <h2 className="text-2xl font-medium">Papers</h2>
-                <span className='text-xs italic'>showing {paperJsonList.length} papers</span>
+    return <div className="flex flex-col h-screen overflow-auto">
+        {/* <div className="flex-1 h-screen overflow-auto flex flex-col"> */}
+        <div className="bg-yellow-100 px-9 py-6 flex-initial">
+            <h2 className="text-2xl font-medium">Papers</h2>
+            <span className='text-xs italic'>showing {paperJsonList.length} papers</span>
+        </div>
+
+        <div className='flex flex-row bg-yellow-50 '>
+            <div className="w-64 pl-9 pr-3 mt-6 h-screen border-r">
+
+                <div className="flex-initial flex flex-row flex-wrap">
+                    {Array.from(labelSet).sort().map(l =>
+                        <div><button key={l} className='text-sm underline mx-1'>{l}</button><span className='text-slate-400'>|</span></div>)
+                    }
+                </div>
             </div>
-            <div className="bg-yellow-50 px-9 py-6 flex-1 flex flex-row flex-wrap">
+
+
+            <div className="px-9 mt-6 flex-1 flex flex-row flex-wrap">
                 {paperJsonList.map(p => <PaperCard key={p.id} paper={p}></PaperCard>)}
             </div>
         </div>
 
+        {/* </div> */}
+
         {/* right side filter */}
-        <div className="flex-initial w-64"> right side filter</div>
+        {/* <div className="flex-initial w-64 flex flex-col border border-left border-black">
+            <div className="bg-yellow-100 px-9 py-6 flex-initial">
+                <h2 className="text-2xl font-medium">Filters</h2>
+            </div>
+            <div className="bg-yellow-50 px-9 py-6 flex-1 flex flex-row flex-wrap">
+                {Array.from(labelSet).sort().map(l =>
+                    <div><button key={l} className='text-sm underline mx-1'>{l}</button> |</div>)
+                }
+            </div>
+        </div> */}
     </div>
 }
--- src/app/papers/paper-card.tsx
+++ src/app/papers/paper-card.tsx
@@ -1,6 +1,17 @@
 import { remark } from 'remark';
 import html from 'remark-html';
 
+const abbrevName = function (fullname: string) {
+    var split_names = fullname.trim().split(" ");
+    let short_name = ""
+
+    for (let i = 0; i < split_names.length - 1; i++) {
+        short_name += split_names[i].charAt(0) + ". "
+    }
+
+    return short_name + split_names[split_names.length - 1];
+};
+
 const PaperCard = async ({
     paper
 }: {
@@ -38,7 +49,7 @@ const PaperCard = async ({
                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="size-4">
                             <path strokeLinecap="round" strokeLinejoin="round" d="M17.982 18.725A7.488 7.488 0 0 0 12 15.75a7.488 7.488 0 0 0-5.982 2.975m11.963 0a9 9 0 1 0-11.963 0m11.963 0A8.966 8.966 0 0 1 12 21a8.966 8.966 0 0 1-5.982-2.275M15 9.75a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
                         </svg>
-                        <span className="text-xs">{a}</span>
+                        <span className="text-xs">{abbrevName(a)}</span>
                     </div>)}
             </div>
 
