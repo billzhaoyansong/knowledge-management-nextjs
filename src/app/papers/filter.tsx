f9982e9d2a0a90cc11f46c44aaa006ffcea2d0f1
Yansong Zhao <bill.zhaoyansong@gmail.com>
2024-12-18 14:43:43 +0800

    fulfill all filter functions

--- src/app/papers/filter.tsx
+++ src/app/papers/filter.tsx
@@ -1,23 +1,99 @@
 'use client'
 
 import { redirect } from "next/navigation"
+import { useState } from "react"
 
-const Filter = () => {
+const Filter = ({ orderBy, searchText, filterLabels, labelsToShow }: {
+    orderBy: string,
+    searchText: string,
+    filterLabels: Set<string>,
+    labelsToShow: Set<string>
+}) => {
 
-    
+    const [_searchText, setSearchText] = useState(searchText)
+
+    function updateOrderBy(newOrderByValue: string) {
+
+        let params = []
+
+        params.push(`orderBy=${newOrderByValue}`)
+
+        if (!(_searchText === ""))
+            params.push(`searchText=${_searchText}`)
+
+        if (filterLabels.size > 0)
+            params.push(Array.from(filterLabels).map(l => `labels=${l}`).join('&'))
+
+        redirect(`/papers?${params.join('&')}`)
+    }
+
+    function updateSearchText() {
+
+        let params = []
+
+        if (!(_searchText === ""))
+            params.push(`searchText=${_searchText}`)
+
+        if (!(orderBy === ""))
+            params.push(`orderBy=${orderBy}`)
+
+        if (filterLabels.size > 0)
+            params.push(Array.from(filterLabels).map(l => `labels=${l}`).join('&'))
+
+        redirect(`/papers?${params.join('&')}`)
+    }
+
+    function updateFilterLabels(label: string) {
+
+        let params = []
+
+        if (!(orderBy === ""))
+            params.push(`orderBy=${orderBy}`)
+
+        if (!(_searchText === ""))
+            params.push(`searchText=${_searchText}`)
+
+        if (filterLabels.has(label))
+            filterLabels.delete(label)
+        else
+            filterLabels.add(label)
+
+        if (filterLabels.size > 0)
+            params.push(Array.from(filterLabels).map(l => `labels=${l}`).join('&'))
+
+        redirect(`/papers?${params.join('&')}`)
+    }
 
     return <div>
         <select name="sortby" id="sortby" className='w-full p-3 bg-yellow-100'
-            onChange={(e) => redirect(`/papers?orderBy=${e.target.value}`)}
+            onChange={(e) => updateOrderBy(e.target.value)}
+            defaultValue={orderBy}
         >
             <option value={'date-desc'}>Date Desc</option>
             <option value={'date-asc'}>Date Asc</option>
         </select>
 
-        <div className='pt-6 flex flex-row'>
-            <input className='w-36 h-7 bg-yellow-100' type="text" name="search" id="search" />
+        <div className='pt-6 flex flex-col'>
+            <input className='w-full h-7 bg-yellow-100' type="text" value={_searchText}
+                placeholder="search text here"
+                onChange={(e) => setSearchText(e.target.value)}
+                onKeyUp={(e) => {
+                    if (e.key === 'Enter')
+                        updateSearchText()
+                }} />
+            <span className="text-xs italic text-slate-400 mt-1">activating search by hitting Enter</span>
 
-            <button className='basis-1/4 text-xs ml-1 p-1 border bg-yellow-100 rounded-md'>Search</button>
+            {/* <button className='basis-1/4 text-xs ml-1 p-1 border bg-yellow-100 rounded-md'>Search</button> */}
+        </div>
+
+        <div className="flex-initial flex flex-row flex-wrap pt-6">
+            {Array.from(labelsToShow).sort().map(l =>
+                <div key={l}>
+                    <button key={l} className='text-sm underline mx-1'
+                        onClick={() => updateFilterLabels(l)}
+                    >{filterLabels.has(l) ? `${l}(x)` : l}</button><span className='text-slate-400'>|</span>
+                </div>)
+            }
         </div>
     </div>
 }
--- src/app/papers/page.tsx
+++ src/app/papers/page.tsx
@@ -1,4 +1,7 @@
 
+// reference: how to read queries in url
+// https://nextjs.org/docs/app/api-reference/file-conventions/page#searchparams-optional
+
 import fs from 'fs';
 import path from 'path';
 import PaperCard from './paper-card';
@@ -11,47 +14,70 @@ export default async function Papers({
     searchParams: Promise<{ [key: string]: string | string[] | undefined }>
 }) {
 
+    // papers to show on screen
+    const _papersToShow = []
+
+    // labels to show on screen
+    const _labelsToShow = new Set<string>()
+
     // fetch params in url
     const params = await searchParams
 
-    // preprocess the labels for filtering
-    const filterLabels = !params.labels ?
+    // preprocess filtering labels from params
+    const _filterLabels = !params.labels ?
         new Set<string>() : typeof params.labels === 'string' ?
             new Set<string>([params.labels]) :
             new Set<string>(params.labels);
 
+    // preprocess orderby from params; if meet multiple orderby params, get the first
+    const _orderBy: string = !params.orderBy ?
+        "" : typeof params.orderBy === 'string' ?
+            params.orderBy :
+            params.orderBy[0];
+
+    // preprocess searchtext from params; if meet multiple searchtext params, get the first
+    const _searchText: string = !params.searchText ?
+        "" : typeof params.searchText === 'string' ?
+            params.searchText :
+            params.searchText[0];
+
     // read paper json file names on server disk
-    const papersDirectoryPath = path.join(process.cwd(), 'src', 'app', 'papers', 'paper-list'); // Specify your directory  
-    const papers = fs.readdirSync(papersDirectoryPath); // Read files in the directory
+    const _papersDiskPath = path.join(process.cwd(), 'src', 'app', 'papers', 'paper-list'); // Specify your directory  
+    const _papersFilenames = fs.readdirSync(_papersDiskPath); // Read files in the directory    
 
-    // container for paper json content
-    const paperContents = []
-
-    // labels to show
-    const labelSet = new Set<string>()
-
-    for (let paper of papers) {
+    // filter papers by filtering labels given in the url
+    for (let paper of _papersFilenames) {
         const paperPath = path.join(process.cwd(), 'src', 'app', 'papers', 'paper-list', paper); // Specify your directory  
 
         const jsonData = await fs.promises.readFile(paperPath, 'utf-8');
         const data = JSON.parse(jsonData);
 
-        if (Object.hasOwn(data, 'labels')) {
+        if (!Object.hasOwn(data, 'labels') || !Object.hasOwn(data, 'title') || !Object.hasOwn(data, 'authors') || !Object.hasOwn(data, 'summaries'))
+            continue
 
-            if (filterLabels.size > 0 && (filterLabels.intersection(new Set(data['labels']))).size < filterLabels.size)
+        if (_searchText !== "") {
+            const _lowerText = _searchText.toLowerCase()
+
+            if (!(data.title.toLowerCase().includes(_lowerText) ||
+                data.summaries[0].toLowerCase().includes(_lowerText) ||
+                data.authors.some((l: string) => l.toLowerCase().includes(_lowerText))))
                 continue
-
-            data['labels'].forEach((label: string) => {
-                labelSet.add(label);
-            });
         }
 
-        paperContents.push(data)
+        // skip if not all filtering labels are shown in local labels
+        if (_filterLabels.size > 0 && (_filterLabels.intersection(new Set(data['labels']))).size < _filterLabels.size)
+            continue
+
+        data['labels'].forEach((label: string) => {
+            _labelsToShow.add(label);
+        });
+
+        _papersToShow.push(data)
 
     }
 
-    // sort before listing
-    paperContents.sort(
+    // sort before showing
+    _papersToShow.sort(
         (a, b) =>
             params.orderBy === "date-asc" ? a.year.localeCompare(b.year) :
                 b.year.localeCompare(a.year)
@@ -68,19 +94,14 @@ export default async function Papers({
             {/* left filters */}
             <div className="w-64 pl-9 pr-6 mt-6 h-screen border-r">
 
-                <span className='text-xs italic'>showing {paperContents.length} papers</span>
+                <span className='text-xs italic'>showing {_papersToShow.length} papers</span>
 
-                <Filter />
-                <div className="flex-initial flex flex-row flex-wrap pt-6">
-                    {Array.from(labelSet).sort().map(l =>
-                        <div><button key={l} className='text-sm underline mx-1'>{l}</button><span className='text-slate-400'>|</span></div>)
-                    }
-                </div>
+                <Filter orderBy={_orderBy} searchText={_searchText} filterLabels={_filterLabels} labelsToShow={_labelsToShow} />
             </div>
 
             {/* right paper cards */}
             <div className="px-9 mt-6 flex-1 flex flex-row flex-wrap">
-                {paperContents.map(p => <PaperCard key={p.id} paper={p}></PaperCard>)}
+                {_papersToShow.map(p => <PaperCard key={p.id} paper={p}></PaperCard>)}
             </div>
         </div>
     </div>
