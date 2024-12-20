f1f730bdd83ac588f9bde2160de66a77d609eaef
Yansong Zhao <bill.zhaoyansong@gmail.com>
2024-12-20 14:42:55 +0800

    add contents and add author tooltip

--- src/app/papers/paper-card.tsx
+++ src/app/papers/paper-card.tsx
@@ -60,7 +60,11 @@ const PaperCard = async ({
                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="size-4">
                             <path strokeLinecap="round" strokeLinejoin="round" d="M17.982 18.725A7.488 7.488 0 0 0 12 15.75a7.488 7.488 0 0 0-5.982 2.975m11.963 0a9 9 0 1 0-11.963 0m11.963 0A8.966 8.966 0 0 1 12 21a8.966 8.966 0 0 1-5.982-2.275M15 9.75a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
                         </svg>
-                        <span className="text-xs">{abbrevName(a)}</span>
+
+                        <div className='relative group flex text-center'>
+                            <span className="text-xs">{abbrevName(a)}</span>
+                            <div className='absolute left-10 -top-10 z-10 transform -translate-x-1/2 mt-2 w-max p-2 text-xs text-white bg-black rounded-md shadow-lg opacity-0 group-hover:opacity-80 transition-opacity duration-200 ease-in-out'>{a}</div>
+                        </div>
                     </div>)}
             </div>
 
