a5bae03ef6dd9f6cb8567e7a4b4d144f59de2a9e
Yansong Zhao <bill.zhaoyansong@gmail.com>
2025-03-02 12:54:23 +0800

    add contents

--- src/app/books/book-card.tsx
+++ src/app/books/book-card.tsx
@@ -14,7 +14,13 @@ export default function BookCard({
     const _sectionNames = fs.readdirSync(_sectionsDiskPath, {
         withFileTypes: true
     }).filter(c => c.isDirectory())
-        .map(c => c.name);
+        .map(c => c.name)
+        .sort(function (a, b) {
+            return a.localeCompare(b, undefined, {
+                numeric: true,
+                sensitivity: 'base'
+            });
+        }); // sort before using the function: ['1', '10', '2']; sort after using the function: ['1', '2', '10']
 
     return <div className="lg:basis-1/4 md:basis-full book-card">
         <div className="h-fit px-3 py-6 mx-1 my-1 border border-1 rounded rounded-lg">
@@ -23,7 +29,7 @@ export default function BookCard({
             <h4 className="text-slate-500 text-md capitalize">{title}</h4>
 
             {/* row 2 */}
-            <ul className="flex flex-col flex-wrap h-64 ">
+            <ul className="flex flex-col flex-wrap ">
                 {_sectionNames.map(n =>
                     <BookCardSection title={title} section={n} key={n} />
                 )}
